00bd64b028b4b4e35e10c53c714efdb999dc03d5
Tomas-Mecius <tmecius@gmail.com>
2021-01-08 07:33:38 +0200

    add final version. generates file with states to learn.

--- main.py
+++ main.py
@@ -2,36 +2,38 @@ import turtle
 import pandas
 
 screen = turtle.Screen()
+screen.setup(width=725, height=491)
 screen.title("U.S. States Game")
 image_shape = "blank_states_img.gif"
 screen.addshape(image_shape)
 turtle.shape(image_shape)
 
-answer = screen.textinput(title="Guess the state", prompt="What's another state?")
-
 data = pandas.read_csv("50_states.csv")
 
-states = data["state"].to_list()
-x_cor = data["x"].to_list()
-y_cor = data["y"].to_list()
+all_states = data["state"].to_list()
 
-data_dict = {"States": states,
-             "x_cor": x_cor,
-             "y_cor": y_cor}
+guessed_states = []
 
-game_is_on = True
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another state?").title()
+    if answer_state == "Exit":
+        missing_state = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_state.append(state)
+        new_data = pandas.DataFrame(missing_state)
+        new_data.to_csv("state_to_learn.csv")
+        break
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.penup()
+        t.hideturtle()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        # t.write(state_data.state.item())
+        t.write(answer_state)
 
-while game_is_on:
-
-    for answer in data_dict["States"]:
-        turtle.goto(x_cor[0], y_cor[0])
-    else:
-        print("Not found")
-
-
-print(data_dict["States"][answer])
-
-turtle.mainloop()
 # Convert the guess to Title letter
 # Check if guess is among 50 states
 # Write correct guess on to the map
